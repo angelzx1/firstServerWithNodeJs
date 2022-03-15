c7905e453197d79a369ced0cd3838e155ab9b074
Nicolas <nicolasrangeldecarvalho@gmail.com>
2022-03-15 14:55:22 -0300

    correcting the error

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 
-app.get("/", (req, res)=>{ req.set('The server is running')})
+app.get("/", (req, res)=>{ res.send('The server is running')})
 
 app.listen(4000, ()=>{
 console.log('Server on port 4000 is running');
